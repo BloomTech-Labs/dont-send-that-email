b9d59ff7bd4e8542c3a8a70eaa7c2ddf491617d6
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-08 14:01:05 -0700

    Added User migration.

---
+++ server/data/migrations/20190108134949_users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table => {
+    table.increments()
+    table
+      .string('username', 128)
+      .notNullable()
+      .unique()
+    table
+      .boolean('subscriber')
+    table.string('password', 128)
+    //We may not need password. But if so, we need it in the db.
+      //.notNullable()
+  })
+  
+};
+
+exports.down = function(knex, Promise) {
+ return knex.schema.dropTableIfExists('users')
+};
