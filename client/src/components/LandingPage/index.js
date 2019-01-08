55aace6e4105206ac9244759adfc6201f5688ca8
Ta1grr <taile602@gmail.com>
2019-01-08 17:09:25 -0600

    Created landing page and is viewable from App component

---
+++ client/src/components/LandingPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LandingPage = () => {
+      return (
+        <div>
+            <nav>
+            <a>Sign In</a>
+            <a>Sign Up</a>
+            </nav>
+            <div>
+                <img src="#" alt="image goes here"/>
+                <p>Blah blah blah(Description goes here)</p>  
+            </div>
+        </div>
+        
+      );
+  }
+
+  export default LandingPage;
