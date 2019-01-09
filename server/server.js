ba3828cddcf4389135f2ab901e244e5d44ca7029
Jared Cuffe <jcuffe@gmail.com>
2019-01-09 14:27:45 -0800

    Adds authentication router

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 
 const express = require("express");
+const authRouter = require("./routers/auth");
 
 const app = express();
+
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
