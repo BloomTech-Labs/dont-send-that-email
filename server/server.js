7d17c5888373f5c8a5add414c8ac08259b185464
Fred <sohnf90@yahoo.com>
2019-01-09 10:10:54 -0800

    Created a dbconfig and errormessage if no username is provided in POST

--- server/server.js
+++ server/server.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
+const db = require('./data/dbconfig.js');
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.get("/users", (req, res) => {
+
+})
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+
+  if(!user.username) {
+    res.status(400).json({ errorMsg: "Please enter a username."})
+  }
+
+})
 
 app.listen(process.env.PORT || 3000, () => console.log("listening"))
