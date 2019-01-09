56bdea5f6774521a4ce973206334b54186c6f257
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-09 10:03:52 -0700

    Wrote in rough endpoints. They do not work right now

--- server/server.js
+++ server/server.js
@@ -6,4 +6,13 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.post('/api/users', (req, res) => {
+  // write the code to post the user.
+
+})
+
+app.get('/api/users', (req, res) => {
+  //here's where we do the get request.
+
+})
 app.listen(process.env.PORT || 3000, () => console.log("listening"))
