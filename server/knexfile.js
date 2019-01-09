113758b5c7b21f55c160977ed804a4ae91cbab53
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-09 13:38:23 -0700

    Put in credentials for heroku postgres DB connection.

--- server/knexfile.js
+++ server/knexfile.js
@@ -35,12 +35,13 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
-      // user:     process.env.DB_USER,
-      // password: process.env.DB_PASSWORD
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   database: process.env.DATABASE_URL
+    //   // user:     process.env.DB_USER,
+    //   // password: process.env.DB_PASSWORD
+    // },
     pool: {
       min: 2,
       max: 10
