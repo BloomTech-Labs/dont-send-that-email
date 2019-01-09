119534ea41fb8b0ca08d4fbe8560ae9832ccd352
Jared Cuffe <jcuffe@gmail.com>
2019-01-09 15:11:40 -0800

    Implements Auth0 authentication.

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,9 +1,49 @@
 const express = require("express")
+const passport = require("passport");
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-  res.send("Logged in");
+// Beginning of login flow. Sends user to Auth0 to authenticate
+router.get("/login", passport.authenticate("auth0", {
+  scope: "openid email profile"
+}), (req, res) => {
+  res.redirect("/")
+});
+
+// Auth0 will redirect the user-agent here after authentication
+// This route will redirect to the client homepage (or possibly client document list)
+router.get("/callback", (req, res, next) => {
+  passport.authenticate("auth0", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+
+    // Initialize a session for the user on the back end
+    // Subsequent requests from the user will have a `req.user` object
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Login was successful, send user-agent to the client
+      res.redirect(process.env.FRONT_END_URL);
+    });
+  })(req, res, next);
+});
+
+// Provides information about the user to the client app, which can then
+// determine what content to show the user
+router.get("/profile", (req, res) => {
+  const data = {};
+  if (req.user) {
+    data.user = req.user;
+  } else {
+    data.err = "No available user session.";
+  }
+  res.json(data);
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,48 @@
+// Configure ENV variables immediately
+require("dotenv").config();
 
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
 const authRouter = require("./routers/auth");
 
+// Configure session
+// TODO - move to config/session.js
+const sessionConfig = {
+  secret: 'CHANGE THIS TO A RANDOM SECRET',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+// Configure Auth0 strategy
+// TODO - move to config/auth.js
+const strategy = new Auth0Strategy({
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: process.env.AUTH0_CALLBACK_URL
+}, (token, refresh, params, profile, done) => {
+  return done(null, profile);
+});
+
+// Configure passport
+// TODO - move to config/passport.js
+passport.use(strategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
 const app = express();
+
+// Initialize middlewares
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+
+if (app.get('env') === 'production') {
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
 
 app.use("/auth", authRouter);
 
@@ -10,4 +50,4 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("listening"))
+app.listen(process.env.PORT || 5000, () => console.log("listening"));
