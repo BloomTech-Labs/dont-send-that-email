2647ddba5afd977e58dad4ac806792ac4fa7a911
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-10 11:15:02 -0700

    Fixed a string problem in the database config file

--- server/data/dbconfig.js
+++ server/data/dbconfig.js
@@ -3,9 +3,8 @@
 // const knexConfig = require('../knexfile.js');
 
 // module.exports = knex(knexConfig.development);
-
-const dbEngine = process.env.DB || development;
+const dbEngine = process.env.DB || 'development';
 const knexConfig = require('../knexfile.js')[dbEngine];
-module.exports = require('knex')(config);
+module.exports = require('knex')(knexConfig);
 
 
