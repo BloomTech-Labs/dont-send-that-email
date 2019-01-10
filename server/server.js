fb305d826f81e80ce595d718001f53cb9d0fbe9c
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-10 10:00:39 -0700

    removed the confilcts in the server.js file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,1 @@
 
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-
-  res.send("Hello, World!");
-});
-
-app.listen(process.env.PORT || 3000, () => console.log("listening"))
