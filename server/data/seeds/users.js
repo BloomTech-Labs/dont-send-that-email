f10d7f23d9726eebe0fa2a8ecbc61a8f1f08129b
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-10 13:45:37 -0700

    Added basic seed data

--- server/data/seeds/users.js
+++ server/data/seeds/users.js
@@ -1,20 +1,17 @@
-const faker = require('faker')
 
-const createFakeUsers = () => {
-  username: faker.name.username()
-
-
-
-}
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      //
       return knex('users').insert([
-
-
+        {id: 1, username: 'chad'},
+        {id: 2, username: 'Tai'},
+        {id: 3, username: 'jared'},
+        {id: 4, username: 'fred'},
+        {id: 5, username: 'Richard'},
+        {id: 6, username: 'thomas'},
       ]);
     });
 };
+
