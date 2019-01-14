0e137ad44952a830e7d144c6103aa98734af5b99
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-14 13:21:13 -0700

    Added dotenv to client app

--- client/src/components/TempWatsonPage/index.js
+++ client/src/components/TempWatsonPage/index.js
@@ -1,10 +1,23 @@
-
+import ToneAnalyzer from 'watson-developer-cloud/tone-analyzer/v3';
 import React, { Component } from 'react';
+import './watson.css';
 
 class WatsonDummy extends Component {
+  constructor() {
+    super()
+      this.state  = {
+        response: []
+      }
+  }
+
+  componentDidMount() {
+    console.log('this is the secret file', process.env.SERVICE_NAME_USERNAME)
+
+  }
+
   render() {
     return (
-      <div>
+      <div className={'watson-box'}>
         <p>Watson response will go here</p>
       </div>
     );
