a7514a3017a9d32995f2ddee4beb83077fbffe16
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-14 15:44:35 -0700

    Removed all the stuff I did on the front end. We do not need it. We will do all requests to watson from the backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from '../src/components/LandingPage';
-import WatsonDummy from '../src/components/TempWatsonPage'
-require('dotenv').config()
 
 class App extends Component {
   render() {
     return (
       <div>
         <LandingPage/>
-        <WatsonDummy />
-
       </div>
     );
   }
