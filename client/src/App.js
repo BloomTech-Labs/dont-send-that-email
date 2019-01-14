cafcf665cafb0a179005a21523b399d14d8eae41
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-14 11:51:43 -0700

    added a watson  dummy component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from '../src/components/LandingPage';
+import WatsonDummy from '../src/components/TempWatsonPage'
 
 class App extends Component {
   render() {
     return (
       <div>
         <LandingPage/>
+        <WatsonDummy />
+
       </div>
     );
   }
---
+++ client/src/components/TempWatsonPage/index.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class WatsonDummy extends Component {
+  render() {
+    return (
+      <div>
+        <p>Watson response will go here</p>
+      </div>
+    );
+  }
+}
+
+export default WatsonDummy;
