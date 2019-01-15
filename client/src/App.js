6ab3f7286154e81888c0f6aa8ac9db687c4a6adb
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-14 17:09:19 -0700

    Added functions to access Watson

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from '../src/components/LandingPage';
+import NewEmail from '../src/components/emailcreate';
 
 class App extends Component {
   render() {
     return (
       <div>
         <LandingPage/>
+        < NewEmail />
       </div>
     );
   }
