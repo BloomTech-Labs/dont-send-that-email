c7e2074344ca227c1962976143baafd483c7fee5
Jared Cuffe <jcuffe@gmail.com>
2019-01-15 15:39:56 -0800

    Implements subscription creation endpoint.

---
+++ server/routers/subscriptions.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const populateUser = require("../middlewares/populateUser");
+const db = require("../data/dbconfig.js");
+const router = express.Router();
+
+router.use(populateUser);
+
+// Required fields: {
+//   transaction_id
+// }
+router.post("/", (req, res) => {
+  const subscription = {
+    user_id: req.user.id,
+    ...req.body
+  }
+
+  db("subscriptions")
+    .insert(subscription)
+    .then(() => res.json({ msg: "Saved transaction to database." }))
+    .catch(err => res.json(err));
+});
