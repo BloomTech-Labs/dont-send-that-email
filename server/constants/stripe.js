2866655284896c4996bd1fcf0eedb58c5e563d04
rverdi642 <rverdi642@gmail.com>
2019-01-16 11:29:08 -0600

    Backend file reflecting moving the visible publishable keys out

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -1,8 +1,8 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_test_yX4Y6x6Aj09AdodtbrIh4CGv'
-    : 'sk_test_yX4Y6x6Aj09AdodtbrIh4CGv';
+    ? process.env.STRIPE_API_KEY
+    : process.env.STRIPE_API_KEY
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
