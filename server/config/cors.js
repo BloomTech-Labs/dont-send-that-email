376f945e20c17984745afafc0d9c25a42ca91b88
Jared Cuffe <jcuffe@gmail.com>
2019-01-17 12:53:00 -0800

    Adds deployed client url to allowed origins.

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const config = {
-  "origin": "http://localhost:3000",
+  "origin": ["http://localhost:3000", "https://dont-send-that-email.netlify.com/"],
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204,
