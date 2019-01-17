7af63b8b6ccf9d8af2df659aed66d311c7420611
Jared Cuffe <jcuffe@gmail.com>
2019-01-17 11:46:38 -0800

    Links to the edit page for the supplied email.

--- client/src/components/DocumentList/Document.js
+++ client/src/components/DocumentList/Document.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Document extends Component {
     render() {
         const { title, addressee } = this.props.email;
         return(
-            <div className="email-card">
-                <h1>{title}</h1>
-                <h2>{addressee}</h2>
-                <div>
-                    <button onClick={this.props.copy}>Copy</button>
+            <Link to={"/email/" + this.props.email.id}>
+                <div className="email-card">
+                    <h1>{title}</h1>
+                    <h2>{addressee}</h2>
+                    <div>
+                        <button onClick={this.props.copy}>Copy</button>
+                    </div>
                 </div>
-            </div>
+            </Link>
         );
     }
 }
