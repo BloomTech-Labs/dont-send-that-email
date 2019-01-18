0dc67c21112b56b54134c0642a273c1fde8f64a4
Jared Cuffe <jcuffe@gmail.com>
2019-01-18 09:08:00 -0800

    Unwraps copy button from within Link.

--- client/src/components/DocumentList/Document.js
+++ client/src/components/DocumentList/Document.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Document extends Component {
-    render() {
-        const { title, addressee } = this.props.email;
-        return(
-            <Link to={"/email/" + this.props.email.id}>
-                <div className="email-card">
-                    <h1>{title}</h1>
-                    <h2>{addressee}</h2>
-                    <div>
-                        <button onClick={this.props.copy}>Copy</button>
-                    </div>
-                </div>
-            </Link>
-        );
-    }
+  render() {
+    const { title, addressee } = this.props.email;
+    return(
+      <div className="email-card">
+        <Link to={"/email/" + this.props.email.id}>
+          <h1>{title}</h1>
+          <h2>{addressee}</h2>
+        </Link>
+          <div>
+            <button onClick={this.props.copy}>Copy</button>
+          </div>
+      </div>
+    );
+  }
 }
