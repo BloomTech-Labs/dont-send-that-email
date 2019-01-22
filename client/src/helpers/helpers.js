900541b9ab7d83b6bdb555953b229e6a482794d5
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-22 11:40:17 -0700

    Working out the limits of the range

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -3,8 +3,8 @@
 //0.865316
 ////0.54706
 watsonNumberCrunch = (number) => {
-
-
+  //if the number is greater than 0.75  return the maximum amount
+  //if it's less than 50. Return the smallest amount
 }
 
 
@@ -12,10 +12,12 @@ watsonNumberCrunch = (number) => {
 
 
 
-modules.export {
+module.exports = {
 
   watsonNumberCrunch
 
 }
 
 console.log(watsonNumberCrunch(0.54706))
+console.log(watsonNumberCrunch(0.865316))
+console.log(watsonNumberCrunch(0.701269))
