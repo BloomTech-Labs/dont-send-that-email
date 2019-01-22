dd34a86ee1434ccd3a58f100323fba8060aba3dd
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-22 11:28:26 -0700

    Working on a helper function for the tone analysis. This will aid the percentages and the colors

---
+++ client/src/helpers/helpers.js
@@ -0,0 +1,21 @@
+
+//0.701269
+//0.865316
+////0.54706
+watsonNumberCrunch = (number) => {
+
+
+}
+
+
+
+
+
+
+modules.export {
+
+  watsonNumberCrunch
+
+}
+
+console.log(watsonNumberCrunch(0.54706))
