8789d1d81793ed67b13a4b893c79e4eaefc63f88
Jared Cuffe <jcuffe@gmail.com>
2019-01-24 14:04:29 -0800

    Fixes analysis persistence bug by using JSON in db operations.

--- server/routers/emails.js
+++ server/routers/emails.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const populateUser = require('../middlewares/populateUser');
-const db = require('../data/dbconfig.js');
+const express = require("express");
+const populateUser = require("../middlewares/populateUser");
+const db = require("../data/dbconfig.js");
 const router = express.Router();
 
 router.use(populateUser);
@@ -9,21 +9,24 @@ router.use(populateUser);
 //    title,
 //    addressee
 // }
-router.post('/', async (req, res) => {
-  console.log('from server post', req.body);
+router.post("/", async (req, res) => {
   try {
     const { email, version } = req.body;
     email.user_id = req.user.id;
     if (email.id) {
       const { title, addressee, id } = email;
-      await db('emails').where({ id }).update({ title, addressee });
+      await db("emails")
+        .where({ id })
+        .update({ title, addressee });
     } else {
-      email.id = (await db('emails').insert(email))[0];
+      email.id = (await db("emails").insert(email))[0];
     }
 
     if (version) {
+      delete version.id; // For saving a new copy of an edited version
       version.email_id = email.id;
-      await db('versions').insert(version);
+      version.tone_analysis = JSON.stringify(version.tone_analysis);
+      await db("versions").insert(version);
     }
 
     res.json({ id: email.id });
@@ -32,20 +35,36 @@ router.post('/', async (req, res) => {
     throw err;
   }
 });
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const count = await db('emails').where({ id }).del();
+  const count = await db("emails")
+    .where({ id })
+    .del();
   res.json({ count });
 });
-router.get('/:id', async (req, res) => {
+
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const email = await db('emails').where({ id }).first();
-  email.versions = await db('versions').where({ email_id: id });
+  const email = await db("emails")
+    .where({ id })
+    .first();
+
+  // Fetch all versions, and parse their tone analyses
+  email.versions = await db("versions")
+    .where({ email_id: id })
+    .then(vs =>
+      vs.map(v => {
+        v.tone_analysis = JSON.parse(v.tone_analysis);
+        return v;
+      })
+    );
+
+  console.log(email.versions);
   res.json({ email });
 });
 
-router.get('/', async (req, res) => {
-  const emails = await db('emails').where({ user_id: req.user.id });
+router.get("/", async (req, res) => {
+  const emails = await db("emails").where({ user_id: req.user.id });
   res.json({ emails });
 });
 
