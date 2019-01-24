3083ff5b1079dd7afdedd8dcd8c5817f7e48a4d5
Jared Cuffe <jcuffe@gmail.com>
2019-01-24 08:48:02 -0800

    Relocates Signout button from BreadCrumb to Sidebar.

--- client/src/components/BreadCrumb/index.js
+++ client/src/components/BreadCrumb/index.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Navbar, Breadcrumb, BreadcrumbItem } from "reactstrap";
 
-const logoutUrl = process.env.REACT_APP_BACKEND_URL + "/auth/logout";
-
 const Crumb = ({ crumb }) => {
   const { name, path } = crumb;
   if (path) {
-    return <BreadcrumbItem><a href={path}>{name}</a></BreadcrumbItem>
-  } 
-  return <BreadcrumbItem active>{name}</BreadcrumbItem>
-}
+    return (
+      <BreadcrumbItem>
+        <a href={path}>{name}</a>
+      </BreadcrumbItem>
+    );
+  }
+  return <BreadcrumbItem active>{name}</BreadcrumbItem>;
+};
 
 const BreadCrumbs = ({ crumbs }) => {
   return (
-    <Navbar>
-      <Breadcrumb>
-        {crumbs.map((crumb, i) => <Crumb key={i} crumb={crumb} />)}
-      </Breadcrumb>
-      <a className="btn btn-secondary btn-signout" href={logoutUrl}>Sign Out</a>
-    </Navbar>
-  )
+    <Breadcrumb>
+      {crumbs.map((crumb, i) => (
+        <Crumb key={i} crumb={crumb} />
+      ))}
+    </Breadcrumb>
+  );
 };
 
 export default BreadCrumbs;
