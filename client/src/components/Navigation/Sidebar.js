66144a3c966b92bc08da1b407b8e7d2874a72ffb
Jared Cuffe <jcuffe@gmail.com>
2019-01-28 15:50:15 -0800

    Refactors markup to include icons.

--- client/src/components/Navigation/Sidebar.js
+++ client/src/components/Navigation/Sidebar.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button, Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { pushRotate as Menu } from "react-burger-menu";
 import "./sideBar.css";
 
@@ -7,22 +9,26 @@ export default class Sidebar extends Component {
   logoutUrl = process.env.REACT_APP_BACKEND_URL + "/auth/logout";
   render() {
     return (
-      <div className="sideBar">
-        <Menu pageWrapId={"page-wrap"}>
-          <Link to="/" className="btn btn-secondary">
-            Home
-          </Link>
-          <Link to="/billing" className="btn btn-secondary">
-            Billing
-          </Link>
-          <Link to="settings" className="btn btn-secondary">
-            Settings
-          </Link>
-          <a className="btn btn-secondary" href={this.logoutUrl}>
-            Sign Out
-          </a>
-        </Menu>
-      </div>
+      <Menu right pageWrapId={"page-wrap"}>
+        <Link to="/">
+          <Button color="danger">
+            <FontAwesomeIcon icon="home" />
+            <span>Home</span>
+          </Button>
+        </Link>
+        <Link to="/profile">
+          <Button color="danger">
+            <FontAwesomeIcon icon="user" />
+            <span>Profile</span>
+          </Button>
+        </Link>
+        <a href={this.logoutUrl}>
+          <Button color="danger">
+            <FontAwesomeIcon icon="sign-out-alt" />
+            <span>Sign Out</span>
+          </Button>
+        </a>
+      </Menu>
     );
   }
 }
