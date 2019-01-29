4e3a2329aa167b2ecb5dbb8770cdcb63e74d861b
Will Kwon <kwontaewoongah@gmail.com>
2019-01-28 18:32:16 -0800

    merged settings and billing route into profile in maincontent

--- client/src/components/MainContent/index.js
+++ client/src/components/MainContent/index.js
@@ -1,37 +1,31 @@
-
-import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
-import Settings from '../Settings';
-import DocumentList from '../DocumentList';
-import Billing from '../Billing';
-import NewEmail from '../emailcreate';
-
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import DocumentList from "../DocumentList";
+import NewEmail from "../emailcreate";
+import Profile from "../Profile";
 
 class MainContent extends Component {
-
-    render() {
-        return(
-            <Switch>
-                <Route exact path='/' name='Home' render={props => (
-                    <DocumentList 
-                        user={this.props.user}
-                    />
-                )}
-                />
-                <Route exact path='/email' component={NewEmail} />
-                <Route exact path='/email/:id' component={NewEmail} />
-                <Route exact path='/billing' name='Billing' render={props => (
-                    <Billing />
-                )}  
-                />
-                <Route exact path='/settings' name='Settings' render={props => (
-                    <Settings />
-                )}
-                />
-            </Switch>
-        )
-    }
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          name="Home"
+          render={(props) => <DocumentList user={this.props.user} />}
+        />
+        <Route exact path="/email" component={NewEmail} />
+        <Route exact path="/email/:id" component={NewEmail} />
+        <Route
+          exact
+          path="/profile"
+          name="Profile"
+          render={(props) => <Profile />}
+        />
+        />
+      </Switch>
+    );
+  }
 }
 
 export default withRouter(MainContent);
-
