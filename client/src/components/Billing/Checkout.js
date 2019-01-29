f21eaa0be70cd3fc0a92134351e7f521982f7737
Will Kwon <kwontaewoongah@gmail.com>
2019-01-28 19:54:24 -0800

    changed button in checkout to purchase 30 day subscription and changed server to reflect that duration

--- client/src/components/Billing/Checkout.js
+++ client/src/components/Billing/Checkout.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import axios from 'axios';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
 
-const PAYMENT_SERVER_URL = process.env.REACT_APP_BACKEND_URL + '/billing';
+const PAYMENT_SERVER_URL = process.env.REACT_APP_BACKEND_URL + "/billing";
 const STRIPE_PUBLISHABLE = process.env.REACT_APP_STRIPE_API_PUBLISH_KEY;
 
-const CURRENCY = 'USD';
+const CURRENCY = "USD";
 
 // All API requests expect amounts to be provided in a currency’s smallest unit.
 // For example, to charge $10 USD, provide an amount value of 1000 (i.e, 1000 cents).
@@ -13,12 +13,12 @@ const CURRENCY = 'USD';
 const fromUSDToCent = (amount) => amount * 100;
 
 const successPayment = (data) => {
-  alert('Payment Successful');
+  alert("Payment Successful");
   console.log(data);
 };
 
 const errorPayment = (data) => {
-  alert('Payment Error');
+  alert("Payment Error");
   console.log(data);
 };
 
@@ -46,7 +46,7 @@ const Checkout = ({ name, description, amount }) => (
     currency={CURRENCY}
     stripeKey={STRIPE_PUBLISHABLE}
   >
-    <button className="btn btn-danger btn-round">Pay Here</button>
+    <button className="btn btn-danger btn-round">Subscribe 30 Days</button>
   </StripeCheckout>
 );
 
