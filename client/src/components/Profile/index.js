f99c75776bd26b685bc68a83b4cbec9b3a9d3ce6
Will Kwon <kwontaewoongah@gmail.com>
2019-01-28 18:39:22 -0800

    made a folder for Profile component and put in billing and settings components

---
+++ client/src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Settings from "../Settings";
+import Billing from "../Billing";
+
+const Profile = () => {
+  return (
+    <div>
+      <Settings />
+      <Billing />
+    </div>
+  );
+};
+export default Profile;
