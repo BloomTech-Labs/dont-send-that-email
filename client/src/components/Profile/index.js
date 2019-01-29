b53972db1e3f49f8f7a5a368f1ad04a3ae91dd98
Will Kwon <kwontaewoongah@gmail.com>
2019-01-28 19:22:44 -0800

    added breadcrumb to profile

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Settings from "../Settings";
 import Billing from "../Billing";
 import Sidebar from "../Navigation/Sidebar";
+import BreadCrumb from "../BreadCrumb";
 
 const Profile = (props) => {
   return (
     <div>
       <Sidebar />
+      <BreadCrumb crumbs={[{ name: "Profile" }]} user={props.user} />
       <Settings user={props.user} />
       <Billing />
     </div>
