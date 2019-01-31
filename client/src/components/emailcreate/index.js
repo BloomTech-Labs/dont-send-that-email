bc2eb12c2012a135aa17191f19b2567eb93edc88
Jared Cuffe <jcuffe@gmail.com>
2019-01-31 00:09:17 -0800

    Replaces CE div with package; solves caret jump on edit.

--- client/src/components/emailcreate/index.js
+++ client/src/components/emailcreate/index.js
@@ -16,7 +16,8 @@ import {
 import BreadCrumb from "../BreadCrumb";
 import Sidebar from "../Navigation/Sidebar";
 import { Link } from "react-router-dom";
-import Editor from "./Editor";
+// import Editor from "./Editor";
+import ContentEditable from "react-contenteditable";
 import Analysis from "./Analysis";
 import "./email.css";
 
@@ -133,7 +134,7 @@ class NewEmail extends Component {
 
   // This is expensive
   editorInput = e => {
-    const text = striptags(e.target.value);
+    const text = e.target.value;
     const versions = this.state.versions;
     versions[this.state.selected_version - 1].text = text;
     this.setState({ versions });
@@ -145,7 +146,10 @@ class NewEmail extends Component {
         axios
           .post(
             process.env.REACT_APP_BACKEND_URL + "/api/watson",
-            { text: this.selectedVersion().text, reqType: "analyze" },
+            {
+              text: striptags(this.selectedVersion().text),
+              reqType: "analyze"
+            },
             { withCredentials: true }
           )
           .then(res => {
@@ -167,6 +171,8 @@ class NewEmail extends Component {
       },
       version: this.selectedVersion()
     };
+
+    body.version.text = striptags(body.version.text);
 
     if (this.props.match.params.id) {
       body.email.id = this.props.match.params.id;
@@ -265,7 +271,12 @@ class NewEmail extends Component {
             </Row>
             <Row>
               <Col md={{ order: 2 }} lg={{ order: 0, size: 8 }}>
-                <Editor html={this.processTone()} onChange={this.editorInput} />
+                <ContentEditable
+                  html={this.processTone()}
+                  onChange={this.editorInput}
+                  className="form-control"
+                  style={{ height: "auto", minHeight: "150px" }}
+                />
               </Col>
               <Col md={{ order: 1 }} lg={{ size: 4 }}>
                 <Analysis
