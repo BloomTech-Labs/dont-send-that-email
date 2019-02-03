39258e662f843225c1a08d5a815765231dfcd3b1
Will Kwon <kwontaewoongah@gmail.com>
2019-02-02 19:49:51 -0800

    changed time related fields in users and subscription to use integer to use Date.now for them changed logic in populateusersmiddleware to account

--- server/data/migrations/20190115141457_create_subscriptions_table.js
+++ server/data/migrations/20190115141457_create_subscriptions_table.js
@@ -1,19 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('subscriptions', table => {
-    table.increments();
-    table.string('transaction_id').notNullable();
-    table.timestamp('date_created').defaultTo(knex.fn.now()).notNullable();
-    table.integer('duration').defaultTo(30);
-    table
-      .integer('user_id')
-      .notNullable()
-      .unsigned()
-      .references('id')
-      .inTable('users');
-  })
+    return knex.schema.createTable("subscriptions", table => {
+        table.increments();
+        table.string("transaction_id").notNullable();
+        table.integer("date_created").notNullable();
+        table.integer("duration").defaultTo(2592000000);
+        table.integer("user_id").notNullable().unsigned().references("id").inTable("users");
+    });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('subscriptions');
+    return knex.schema.dropTableIfExists("subscriptions");
 };
--- server/middlewares/populateUser.js
+++ server/middlewares/populateUser.js
@@ -1,90 +1,87 @@
-const moment = require("moment");
 const db = require("../data/dbconfig");
+const month = 2592000000;
+
 const populateUser = async (req, res, next) => {
-  if (!req.user) {
-    return res.status(400).json({ err: "No user logged in." });
-  }
-
-  try {
-    // Look up user in db using email address from session
-    const emailaddress = req.user.emails[0].value;
-    const users = await db("users").where({ emailaddress });
-    req.user = users[0];
-
-    // Look up the latest subscription for the user
-    const subscription = await db("subscriptions")
-      .where({ user_id: req.user.id })
-      .orderBy("date_created", "desc")
-      .first();
-    if (subscription) {
-      const subscriptionDetails = isSubscriptionActive(subscription);
-      if (subscriptionDetails[0]) {
-        req.user.subscribed = subscriptionDetails[0];
-        req.user.subscriptionEnd = subscriptionDetails[1];
-      }
+    if (!req.user) {
+        return res.status(400).json({ err: "No user logged in." });
     }
-    // console.log("Delete this: [populateUser] - found ", req.user);
-    if (req.body.reqType === "send") {
-      if (req.user.subscribed) {
-        req.body.username = req.user.username;
-      } else {
-        throw "Request failed with status code 429";
-      }
-    }
-    if (req.body.reqType === "analyze") {
-      if (req.user.subscribed) {
-        next();
-      } else {
-        let verified;
-        if (
-          req.user.currentCycleStart === null &&
-          req.user.analysesCount === null
-        ) {
-          verified = await db("users")
-            .where({ id: req.user.id })
-            .update({ analysesCount: 1, currentCycleStart: new Date() });
-        } else {
-          const cycleStart = moment(req.user.cycleStart);
-          const cycleEnd = moment(req.user.cycleStart).add(30, "days");
-          if (moment().isBetween(cycleStart, cycleEnd)) {
-            verified = await db("users")
-              .where({ id: req.user.id })
-              .update({ analysesCount: 1, currentCycleStart: new Date() });
-          } else {
-            if (req.user.analysesCount < 100) {
-              verified = await db("users")
-                .where({ id: req.user.id })
-                .update({ analysesCount: req.user.analysesCount + 1 });
+
+    try {
+        // Look up user in db using email address from session
+        const emailaddress = req.user.emails[0].value;
+        const users = await db("users").where({ emailaddress });
+        req.user = users[0];
+
+        // Look up the latest subscription for the user
+        const subscription = await db("subscriptions")
+            .where({ user_id: req.user.id })
+            .orderBy("date_created", "desc")
+            .first();
+        if (subscription) {
+            const subscriptionDetails = isSubscriptionActive(subscription);
+            if (subscriptionDetails[0]) {
+                req.user.subscribed = subscriptionDetails[0];
+                req.user.subscriptionEnd = subscriptionDetails[1];
             }
-          }
         }
-        if (verified === 1) {
-          next();
+        // console.log("Delete this: [populateUser] - found ", req.user);
+        if (req.body.reqType === "send") {
+            if (req.user.subscribed) {
+                req.body.username = req.user.username;
+            } else {
+                throw "Request failed with status code 429";
+            }
+        }
+        if (req.body.reqType === "analyze") {
+            if (req.user.subscribed) {
+                next();
+            } else {
+                let verified;
+                if (req.user.currentCycleStart === null && req.user.analysesCount === null) {
+                    verified = await db("users")
+                        .where({ id: req.user.id })
+                        .update({ analysesCount: 1, currentCycleStart: Date.now() });
+                } else {
+                    const now = Date.now();
+                    const currentCycleEnd = req.user.currentCycleStart + month;
+                    if (now >= currentCycleEnd) {
+                        verified = await db("users")
+                            .where({ id: req.user.id })
+                            .update({ analysesCount: 1, currentCycleStart: Date.now() });
+                    } else {
+                        if (req.user.analysesCount < 100) {
+                            verified = await db("users")
+                                .where({ id: req.user.id })
+                                .update({ analysesCount: req.user.analysesCount + 1 });
+                        }
+                    }
+                }
+                if (verified === 1) {
+                    next();
+                } else {
+                    throw "Request failed with status code 429";
+                }
+            }
         } else {
-          throw "Request failed with status code 429";
+            next();
         }
-      }
-    } else {
-      next();
+    } catch (err) {
+        if (err == "Request failed with status code 429") {
+            res.status(429).json(err);
+        } else {
+            res.status(500).json(err);
+        }
     }
-  } catch (err) {
-    if (err == "Request failed with status code 429") {
-      res.status(429).json(err);
-    } else {
-      res.status(500).json(err);
-    }
-  }
 };
 
 const isSubscriptionActive = subscription => {
-  // Force moment to treat datestring from the database as UTC
-  // TODO: fix this in the knex configuration
-  const datestring = subscription.date_created;
-
-  // Check to see if user is within the subscription window
-  const start = moment(datestring);
-  const end = moment(datestring).add(subscription.duration, "days");
-  return [moment().isBetween(start, end), end];
+    // Force moment to treat datestring from the database as UTC
+    // TODO: fix this in the knex configuration
+    const start = subscription.date_created;
+    // Check to see if user is within the subscription window
+    const end = subscription.date_created + subscription.duration;
+    const now = Date.now();
+    return [ now >= start && now <= end, end ];
 };
 
 module.exports = populateUser;
